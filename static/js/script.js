3067359ba3f239872f836ae38e8874abf70ba64d
cyph <89238641+cyphgithub@users.noreply.github.com>
2021-12-08 23:29:36 -0600

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -5,3 +5,5 @@ ScrollReveal().reveal("#container ul", {
 duration: 1e3,
 delay: 750
 })
+console.log("%ccyph%c.pw", "font-size:128px;background:transparent;color:#eeeeee;font-family:'Raleway',Helvetica Neue,Helvetica,sans-serif;text-shadow: 1px 1px 4px #111111", "font-size:128px;background:transparent;color:#6cc312;font-family:'Raleway',Helvetica Neue,Helvetica,sans-serif;text-shadow: 1px 1px 4px #111111", "font-size:32px;color:#b29700;font-family:'Raleway';background:transparent;text-shadow: 1px 1px 4px #111111")
+console.log("your mom")
